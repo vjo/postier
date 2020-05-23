198322dd400483f31b99483f89396e664d3402c1
vjo <victor.jolissaint@gmail.com>
2020-05-23 21:30:42 +0100

    Add favicon and web manifest

--- pages/_document.js
+++ pages/_document.js
@@ -10,6 +10,24 @@ export default class MyDocument extends Document {
         <Head>
           {/* PWA primary color */}
           <meta name="theme-color" content={theme.palette.primary.main} />
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/favicon-32x32.png"
+            sizes="32x32"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            href="/favicon-16x16.png"
+            sizes="16x16"
+          />
+          <link rel="manifest" href="/manifest.webmanifest" />
           <link rel="icon" href="/favicon.ico" />
           <link
             rel="stylesheet"
